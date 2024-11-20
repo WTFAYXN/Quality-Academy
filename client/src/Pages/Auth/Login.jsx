104c71aa4d4d8b339cd17f9ed3887f7609151d58
Ayan <132146120+WTFAYXN@users.noreply.github.com>
2024-11-20 07:27:36 +0000

    auth added

---
+++ client/src/Pages/Auth/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login =() =>{
+    return(
+        <>
+
+        <div>Hello</div>
+        </>
+
+)
+
+}
+
+export default Login
---
+++ client/src/Pages/Auth/Signup.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Signup =() =>{
+    return(
+        <>
+
+        <div>Hello</div>
+        </>
+
+)
+
+}
+
+export default Signup
