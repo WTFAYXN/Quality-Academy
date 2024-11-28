eab7fca22158d013f83bdf4ea513d3e6f4105276
Ayan <132146120+WTFAYXN@users.noreply.github.com>
2024-11-28 13:09:26 +0000

    footer adjustment

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
         <h2 className="heading heading-sub">Subscribe & Connect</h2>
         <form>
           <label className='newsletter-label' htmlFor="newsletter">Subscribe to our newsletter</label>
-          <input className='newsletter-email' type="email" id="newsletter" />
+          <input className='newsletter-email' type="email" id="newsletter" placeholder='Enter your email' />
           <button className='newsletter' type="submit"></button>
         </form>
 
