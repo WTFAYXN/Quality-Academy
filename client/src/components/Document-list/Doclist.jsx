66a9afdbfdcfcd84b5414596dd2d8d813aafea4c
HuzaifaAnsari0 <ayansayadwix@gmail.com>
2024-12-30 10:00:38 +0530

    category added to document list

--- client/src/components/Document-list/Doclist.jsx
+++ client/src/components/Document-list/Doclist.jsx
@@ -6,14 +6,17 @@ const Doclist = ({ filteredResources, handleDelete, isAdmin }) => {
     <ul className="document-list">
       <div className="document-header">
         <li id="title" className="title">Title</li>
+        <li id="category" className="category">Category</li>
         <li id="uploadedBy" className="uploadedBy">Uploaded by</li>
         <li id="uploadDate" className="uploadDate">Upload Date</li>
         <li id="fileType" className="fileType">File Type</li>
         <li id="download" className="download">Download</li>
+        {isAdmin && <li id="delete" className="delete">Delete</li>}
       </div>
       {filteredResources.map((resource) => (
         <div key={resource._id} className="documents">
           <li className="title">{resource.title}</li>
+          <li className="category">{resource.category}</li>
           <li className="uploadedBy">{resource.uploadedBy.name}</li> {/* Use resource.uploadedBy.name */}
           <li className="uploadDate">
             {new Date(resource.createdAt).toLocaleDateString()}
